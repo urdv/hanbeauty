3a943561ff9d11282709915b44b72144b7372fec
kimyh <kyh90495392@gmail.com>
2020-03-20 15:34:34 +0900

    header 수정 완료 #3

--- sub.php
+++ sub.php
@@ -8,8 +8,7 @@
     <main id="content">
         <!-- page-header -->
         <div class="page-header">
-            <!--<div class="page-intro">
-                <div class="figure" style="background-image: url(/assets/images/sub/bg_page_header.jpg);"></div>
+            <div class="page-intro">
                 <div class="video-area">
                     <video class="video" autoplay loop muted preload="metadata">
                         <source src="/assets/movie/1027606283-preview.mp4" type="video/mp4">
@@ -18,30 +17,40 @@
                 <div class="details">
                     <h2>한미인의원소개</h2>
                 </div>
-            </div>-->
+            </div>
         </div>
         
         <nav class="depth-2">
             <div class="container">
                 <ul>
-                    <li class="active"><a href="#">메뉴 1-1</a></li>
-                    <li><a href="#">메뉴 1-2</a></li>
-                    <li><a href="#">메뉴 1-3</a></li>
-                    <li><a href="#">메뉴 1-4</a></li>
-                    <li><a href="#">메뉴 1-5</a></li>
-                    <li><a href="#">메뉴 1-6</a></li>
+                    <li class="active"><a href="#"><span>탄력/리프팅/V라인</span></a></li>
+                    <li><a href="#"><span>기미/색소/홍조</span></a></li>
+                    <li><a href="#"><span>보톡스</span></a></li>
+                    <li><a href="#"><span>여드름/필링/한관종</span></a></li>
+                    <li><a href="#"><span>흉터/모공/튼살</span></a></li>
+                    <li><a href="#"><span>제모</span></a></li>
+                    <li><a href="#"><span>비만/체형관리</span></a></li>
+                    <li><a href="#"><span>피부/항노화주사</span></a></li>
+                    <li><a href="#"><span>예방주사&amp;태반</span></a></li>
+                    <li><a href="#"><span>스킨케어(별관)</span></a></li>
+                    <li><a href="#"><span>필링(별관)</span></a></li>
                 </ul>
             </div>    
         </nav>
         <nav class="depth-3">
             <div class="container">
                 <ul>
-                    <li class="active"><a href="#">메뉴 1-1-1</a></li>
-                    <li><a href="#">메뉴 1-1-2</a></li>
-                    <li><a href="#">메뉴 1-1-3</a></li>
-                    <li><a href="#">메뉴 1-1-4</a></li>
-                    <li><a href="#">메뉴 1-1-5</a></li>
-                    <li><a href="#">메뉴 1-1-6</a></li>
+                    <li class="active"><a href="#">레이저 토닝</a></li>
+                    <li><a href="#">I2PL</a></li>
+                    <li><a href="#">레블라이트si</a></li>
+                    <li><a href="#">레블라이트si+하이할라오 12분</a></li>
+                    <li><a href="#">피코 토닝</a></li>
+                    <li><a href="#">젠틀맥스 프로 제네시스 토닝</a></li>
+                    <li><a href="#">젠틀맥스 프로 알렉스 토닝</a></li>
+                    <li><a href="#">젠틀맥스 프로 잡티</a></li>
+                    <li><a href="#">브이빔 퍼펙타</a></li>
+                    <li><a href="#">비타민 이온토 재생관리</a></li>
+                    <li><a href="#">4가지 토닝을 한번에</a></li>
                 </ul>
             </div>    
         </nav>
@@ -56,7 +65,7 @@
             });
         })(jQuery);
     </script>
-    <!--script>
+    <script>
         (function($){
             // sub body class add (header scroll 효과적용)
             $(document).ready(function(){
@@ -72,6 +81,6 @@
                 $('#lnb .dropdown').toggleClass('show');
             });
         })(jQuery);
-    </script-->
+    </script>
 </body>
 </html>
